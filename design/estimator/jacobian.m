0dc5dd29b1bbf3e2a053bd3d49c64716be551be5
Finn Breuer <finn@tcrobotics.eu>
2024-11-26 00:18:03 -0500

    Simplified function

--- design/estimator/jacobian.m
+++ design/estimator/jacobian.m
@@ -1,24 +1,17 @@
-function [Jx, Ju] = jacobian(f, t, x, u, h)
-    % Computes the Jacobian matrix of a vector function using complex-step
-    % differentiation
+function [Jx] = jacobian(f, t, x, u, h, phase)
+    % Computes the Jacobian matrix of a vector function using complex-step differentiation
+    % Inputs: ODE function f, time t, state x, step size h
+    % Outputs: Jacobian matrix del_f/del_x
 
-    y = f(t, x, u);
+    n = length(x); % size of input
+    y = f(t, x, u, phase);
+    l = length(y); % size of output
     
-    n = length(x);
-    m = length(u);
-    l = length(y);
-    
-    Jx = zeros(l,n);
+    Jx = zeros(l,n); % prep Jacobian array
+
     for k = 1:n
         one = zeros(n,1);
         one(k) = 1;
-        Jx(:,k) = imag(f(t, x+1i*h*one, u)) / h; 
-    end
-
-    Ju = zeros(l,m);
-    for k = 1:m
-        one = zeros(m,1);
-        one(k) = 1;
-        Ju(:,k) = imag(f(t, x, u+1i*h*one)) / h; 
+        Jx(:,k) = imag(f(t, x+1i*h*one, u, phase)) / h; 
     end
 end
