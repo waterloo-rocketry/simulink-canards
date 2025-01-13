cd94ab202d32e043e4eb20f6680478da12644149
Finn Breuer <finn@tcrobotics.eu>
2025-01-13 23:31:09 +0100

    Simplified prediction P compute, and introduced new correction P to avoid singularities. Does not work yet

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -14,19 +14,18 @@ function [x_new, P_new] = ekf_algorithm(x, P, u, y, t, Q, R, T, step)
     %%% x_dot = model_f(x, u);
     % [~,x_solver] = ode45(@(t, x_var)model_f(t, x_var, u), 0:step:T, x); % RK45 (DoPri)
     % x_new = x_solver(end,:)';
-    [x_new, ~] = solver_vector(@(t_, x_, u_)model_f(t_, x_, u_), T, step, 0, x, u); % RK4
+    [x_new] = solver_vector(@(t_, x_, u_)model_f(t_, x_, u_), T, step, 0, x, u); % RK4
 
     %%% compute Jacobians (using complex-step differentiation)
-    F1 = jacobian(@model_f, t, x, u, step); 
-    F2 = jacobian(@model_f, t+T, x_new, u, step);
+    F = jacobian(@model_f, t, x, u, step); 
+    % F2 = jacobian(@model_f, t+T, x_new, u, step);
     
     %%% solve IVP for P
     %%% P_dot = F*P + P*F'+ Q
-    P_dot = F1*P + P*F1'+ Q;
+    P_dot = F*P + P*F'+ Q;
     P2 = P + T*P_dot;
-    P_new = P + T/2*( P_dot + (F2*P2 + P2*F2'+ Q) ); % Heuns method
-    % does not work yet [P_new, ~] = solver_matrix(@(t_, P_)F1*P + P*F1'+ Q, T, step, 0, P, u); % Heuns method
-    
+    P_new = P + T/2*( P_dot + (F*P2 + P2*F'+ Q) ); % Heuns method
+   
     x = x_new; P = P_new;
 
     %% Correction
@@ -46,5 +45,6 @@ function [x_new, P_new] = ekf_algorithm(x, P, u, y, t, Q, R, T, step)
 
     %%% correct state and covariance estimates
     x_new = x + K*innovation;
-    P_new = (eye(length(P)) - K*H ) * P;
+    % P_new = (eye(length(P)) - K*H ) * P;
+    P_new = (eye(length(P))-K*H)*P*(eye(length(P))-K*H)' + K*R*K';
 end
