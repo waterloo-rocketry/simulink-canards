19d4242cb29231bcc3f34a561d111dd62325dfe2
Finn Breuer <finn@tcrobotics.eu>
2025-01-13 23:31:38 +0100

    changes to tuning matrices

--- design/estimator/estimator_module.m
+++ design/estimator/estimator_module.m
@@ -9,8 +9,9 @@ function [xhat] = estimator_module(timestamp, omega, mag, accel, baro, cmd)
 
     % initialize at beginning
     if isempty(P)
-        x = ekf_initialization([omega;mag;accel;baro]);
+        x = initializor([omega;mag;accel;baro]);
         P = zeros(length(x));
+        t = 0;
     end 
     
     %% concoct y and u
@@ -19,14 +20,14 @@ function [xhat] = estimator_module(timestamp, omega, mag, accel, baro, cmd)
     u = [cmd; accel];
 
     %% set parameters for EKF
-    step = 0.0001; % step size for RK45 and Jacobian
-    T = timestamp - t; % end time for RK45 and Improved Euler
+    step = 0.001; % step size for RK4 and Jacobian
+    T = timestamp - t; % end time for RK4 and Improved Euler
     t = timestamp;
 
     Q = diag([ 1e-3*ones(1,4), 10,10,10, 0.1,0.1,0.1, 1,  0.01, 1e-4]); % Square 13 matrix, tuning for prediction E(noise)
     %%% x = [q(4), w(3), v(3), alt(1), Cl(1), delta(1)]
 
-    R = diag([1e-3*ones(1,3), 1*ones(1,3), 0.01, 0.1]); % Square 8*a matrix (a amount of sensors), tuning for measurement E(noise)
+    R = diag([1e-3*ones(1,3), 1*ones(1,3), 0.01]); % Square 8*a matrix (a amount of sensors), tuning for measurement E(noise)
     %%% y = [W(3), Mag(3), P(1), T(1)]
 
     % compute new estimate with EKF
