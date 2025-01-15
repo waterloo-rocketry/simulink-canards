03ffb3a8f71cb45b8d3f37268d4e9635f4dae53b
Finn Breuer <finn@tcrobotics.eu>
2025-01-15 16:41:55 +0100

    quaternions are now stable-ish

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -10,19 +10,23 @@ function [x_new, P_new] = ekf_algorithm(x, P, u, y, t, Q, R, T, step)
     % Uses continuous-time model f and solves for state estimate using RK45
     % Solves for covariance estimate using Improved Euler
     
-    %%% solve IVP for x
-    %%% x_dot = model_f(x, u);
+    %%% solve IVP for x: x_dot = f(x, u)
     [x_new] = solver_vector(@model_f, T, step, t, x, u); % RK4
 
-    %%% compute Jacobians (using complex-step differentiation)
+    %%% compute Jacobians: F = df/dx
     F = jacobian(@model_f, t, x, u, step); 
     F2 = jacobian(@model_f, t+T, x_new, u, step);
     
-    %%% solve IVP for P
-    %%% P_dot = F*P + P*F'+ Q
-    P_dot = F*P + P*F'+ Q;
-    P2 = P + T*P_dot;
-    P_new = P + T/2*( P_dot + (F2*P2 + P2*F2'+ Q) ); % Heuns method
+    %%% solve IVP for P: P_dot = F*P + P*F'+ Q
+    %%% Heuns method
+    % P_dot = F*P + P*F'+ Q;
+    % P2 = P + T*P_dot;
+    % P_new = P + T/2*( P_dot + (F2*P2 + P2*F2'+ Q) ); 
+    %%% discrete variant
+    A = eye(length(x)) + F*T;
+    P_new = A*P*A' + Q; %A*Q*A';
+
+    %%% a-priori estimates
     x = x_new; P = P_new;
 
     %% Correction
@@ -43,8 +47,8 @@ function [x_new, P_new] = ekf_algorithm(x, P, u, y, t, Q, R, T, step)
     %%% correct state and covariance estimates
     x_error = K*innovation;
     x_new = x + x_error;
-    P_new = (eye(length(x)) - K*H ) * P;
-    % P_new = (eye(length(P))-K*H)*P*(eye(length(P))-K*H)' + K*R*K'; % allegedly more stable
+    % P_new = (eye(length(x)) - K*H ) * P;
+    P_new = (eye(length(P))-K*H)*P*(eye(length(P))-K*H)' + K*R*K'; % allegedly more stable
 
     %% troubleshooting
     % P_pred = P_new(1:11,1:11)
