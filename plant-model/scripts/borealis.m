485069d25637beb6c258fad00a0adb46018320d4
Finn Breuer <finn@tcrobotics.eu>
2025-01-17 13:52:40 +0100

    Improved servo model: nonlinear actuator with backlash

--- plant-model/scripts/borealis.m
+++ plant-model/scripts/borealis.m
@@ -1,14 +1,24 @@
-%% Reference Geometry
-%Reference parameters
-Lr = 0.152; % reference length [m]
-Ar = pi * (Lr^2) / 4; % reference area [m^2]
+%% OR Simulation Output Data
+or_data = readtable("plant-model\Data\Borealis\Borealis_flight_no_wind.csv");
 
-%Sensor parameters
+%% Sensor parameters
 samplingrate = 0.01; % sampling period of 
 Ls1 = [-2.4;0;0]; % mounting location of IMU 1 relative CG
 Ss1 = eye(3); % mounting orientation of IMU 1 relative body frame
 location = [10; 43.47; -80.54]; % launch location on earth. Altitude, Latitude, Longitude
-        
+
+%% Actuator parameters
+act_freq = 58; % natural frequency, approx 1/timeconstant
+act_damping = 0.9; % damping ratio
+act_backlash = 1; % play in deg
+act_anglelimit = 15; % max deflection in deg
+act_ratelimit = 600; % max rate in deg/s
+
+%% Reference Geometry
+%Reference parameters   
+Lr = 0.152; % reference length [m]
+Ar = pi * (Lr^2) / 4; % reference area [m^2]
+
 %Nosecone parameters
 logiva = 0.638; % nosecone length [m]
 r0 = 0.152 / 2; % nosecone radius [m]
@@ -38,13 +48,11 @@ Cr_canard = 40 / 1000;
 Ct_canard = 40 / 1000; % "The tip is the size of the root to take advantage of the fact that the further away from the rocket, the greater the moment arm."
 span_canard = 80 / 1000;
 arm_canard = 10/1000; % Moment arm from fin to fuselage
-alfa_canard = degtorad(0); % Canard maximum angle of attack
+alfa_canard = deg2rad(0); % Canard maximum angle of attack
 pos_canard = -(558.29 + 40)/1000;
 
 rail_angle = deg2rad(5);
 
-% OR Simulation Output Data
-or_data = readtable("plant-model\Data\Borealis\Borealis_flight_no_wind.csv")
 
 %% MCI
 % Wet
