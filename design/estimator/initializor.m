82b6f53262919a1d782c8efddc98188b5acfdfde
Finn Breuer <finn@tcrobotics.eu>
2025-02-02 23:55:54 +0100

    Support for adaptive IMU counts, measurements and bias now as matrices

--- design/estimator/initializor.m
+++ design/estimator/initializor.m
@@ -1,11 +1,14 @@
 function [x_init, bias, out] = initializor(meas)
     % Computes inital state and covariance estimate for EKF, and bias values for the IMU
     % Uses all available sensors: Gyroscope W, Magnetometer M, Accelerometer A, Barometer P
+    % Outputs: initial state, sensor bias matrix
 
     persistent sensors; % remembers from last iteration
+    if isempty(sensors)
+        sensors = meas;
+    end
 
-    %%% decompose measurement vector
-    W = meas(1:3); M = meas(4:6); A = meas(7:9); P = meas(10);
+    global IMU_select
 
     %% load parameters
     persistent param
@@ -19,31 +22,39 @@ function [x_init, bias, out] = initializor(meas)
     P_B = 101325; % troposphere base pressure
     k_B = 0.0065; % troposphere base lapse rate
 
-    %% Bias determination
-    y = meas(1:10);
-    if isempty(sensors)
-        sensors = y;
-    end
-    %%% lowpass to attenuate sensor noise
+    %% lowpass filter
     alpha = 0.005; % low pass time constant
-    sensors = sensors + alpha*(y-sensors); % lowpass filter
+    %%% lowpass to attenuate sensor noise
+    sensors = sensors + alpha*(meas-sensors);
+
+    %% decompose measurement matrix
+    A = sensors(1:3,:); W = sensors(4:6,:); M = sensors(7:9,:); P = sensors(10,:);
 
     %% State determination
-    %%% gravity vector in body-fixed frame
-    a = param.S1*sensors(7:9);
+    
+    %%% average specific force of selected sensors
+    a = zeros(3,1);
+    for i = 1:size(meas,2)
+        a = a + A(:, i) /size(A,2);
+    end
 
-    %%% compute launch attitude quaternion
+    %%% gravity vector in body-fixed frame
     psi = atan(-a(2)/a(1)); % rail yaw angle
     theta = atan(a(3)/a(1)); % rail pitch angle
+
+    %%% compute launch attitude quaternion
     q = [cos(psi/2)*cos(theta/2); 
          -sin(psi/2)*sin(theta/2);
          cos(psi/2)*sin(theta/2);
          sin(psi/2)*cos(theta/2)];
 
     %%% compute altitude
-    P = sensors(10);
+    p = 0;
+    for i = 1:size(meas,2)
+        p = p + P(i) / size(meas,2);
+    end
     % alt = -log(P/P_B)*air_R*T_B/g0;
-    alt = T_B/k_B * (1- (P/P_B)^(air_R*k_B/g0) );
+    alt = T_B/k_B * (1- (p/P_B)^(air_R*k_B/g0) );
     
     %%% set constant initials
     w = [0; 0; 0];
@@ -55,16 +66,25 @@ function [x_init, bias, out] = initializor(meas)
     x_init = [q;w;v;alt;Cl;delta];
 
     %% Bias output
-    bias = zeros(9,1);
+    bias = zeros(size(meas)); % prep bias matrix
     
+    %%% accelerometer
+    bias(1:3,:) = A;
+
     %%% gyroscope
-    bias(1:3) = sensors(1:3);
-    bias(7:9) = sensors(7:9);
+    bias(4:6,:) = W;
     
-    %%% compute earth magnetic field
-    S = quaternion_rotmatrix(q); % launch attitude
-    M_E = (S')*(param.S1')*sensors(4:6); % sensorframe -> body-fixed -> earth-flat
-    bias(4:6) = M_E;
+    %%% earth magnetic field
+    S = quaternion_rotmatrix(q); % launch attitude    
+    M_E = zeros(3, norm(IMU_select, 1));
+    for i = 1:size(meas,2)
+        M_E(:,i) = (S')*M(:, i); % sensorframe -> body-fixed -> earth-flat
+        % TODO: add iron corrections
+    end
+    bias(7:9, :) = M_E;
+
+    %%% barometer
+    bias(10,:) = P;
 
     %% troubleshooting
     out = [sensors; M_E; psi; theta];
