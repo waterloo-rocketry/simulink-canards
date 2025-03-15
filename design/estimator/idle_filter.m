7025b113034643b66a876c8cc31d8d9ec2660202
Finn Breuer <finn@tcrobotics.eu>
2025-03-15 23:21:14 +0100

    renamed to clear confusion

---
+++ design/estimator/idle_filter.m
@@ -0,0 +1,126 @@
+function [x_init, bias_1, bias_2, bias_3] = idle_filter(IMU_1, IMU_2, IMU_3)
+    % Computes inital state and covariance estimate for EKF, and bias values for the IMU
+    % Uses all available sensors: Gyroscope W, Magnetometer M, Accelerometer A, Barometer P
+    % Outputs: initial state, sensor bias matrix
+
+    global IMU_select
+
+    %% parameters
+    persistent param
+    if isempty(param)
+        param = load("model\model_params.mat");
+    end
+ 
+
+    %% lowpass filter
+
+    alpha = 0.005; % low pass time constant
+    persistent filtered_1 filtered_2 filtered_3; % remembers from last iteration
+    % filtered = filtered + alpha*(measured-filtered);
+
+    if IMU_select(1) == 1 % if IMU_i alive
+        if isempty(filtered_1)
+            filtered_1 = IMU_1(1:10);
+        end
+        filtered_1 = filtered_1 + alpha * (IMU_1(1:10) - filtered_1);
+    end
+    if IMU_select(2) == 1
+        if isempty(filtered_2)
+            filtered_2 = IMU_2(1:10);
+        end
+        filtered_2 = filtered_2 + alpha * (IMU_2(1:10) - filtered_2);
+    end
+    if IMU_select(3) == 1
+        if isempty(filtered_3)
+            filtered_3 = IMU_3(1:10);
+        end
+        filtered_3 = filtered_3 + alpha * (IMU_3(1:10) - filtered_3);
+    end
+
+
+    %% State determination
+    
+    %%% average specific force of selected sensors
+    a = zeros(3,1); % acceleration a
+    if IMU_select(1) == 1 % only add alive IMUs to average
+        a = a + filtered_1(1:3);
+    end
+    if IMU_select(2) == 1
+        a = a + filtered_2(1:3);
+    end
+    if IMU_select(3) == 1
+        a = a + filtered_3(1:3);
+    end
+    a = a / norm(IMU_select, 1); % divide by number of alive IMUs
+
+    %%% gravity vector in body-fixed frame
+    psi = atan(-a(2)/a(1)); % rail yaw angle
+    theta = atan(a(3)/a(1)); % rail pitch angle
+
+    %%% compute launch attitude quaternion
+    q = [cos(psi/2)*cos(theta/2); 
+         -sin(psi/2)*sin(theta/2);
+         cos(psi/2)*sin(theta/2);
+         sin(psi/2)*cos(theta/2)];
+
+    %%% compute altitude
+    p = 0; % barometric pressure p
+    if IMU_select(1) == 1 % only add alive IMUs to average
+        p = p + filtered_1(10);
+    end
+    if IMU_select(2) == 1
+        p = p + filtered_2(10);
+    end
+    if IMU_select(3) == 1
+        p = p + filtered_3(10);
+    end
+    p = p / norm(IMU_select, 1); % divide by number of alive IMUs
+
+    %%% current altitude
+    alt = model_altdata(p);
+    
+    %%% set constant initials
+    w = [0; 0; 0]; % stationary on rail
+    v = [0; 0; 0]; % stationary on rail
+    Cl = param.Cl_alpha; % from model parameters
+    delta = 0; % controller sets canards to zero due to flight phase
+
+    %%% conconct state vector
+    x_init = [q; w; v; alt; Cl; delta];
+
+    %% Bias determination
+
+    bias_1 = zeros(10, 1); % declare bias vectors
+    bias_2 = zeros(10, 1);
+    bias_3 = zeros(10, 1);
+    
+    %%% accelerometer
+    % did not add accelerometer bias determination yet, leave out for now
+
+    %%% gyroscope
+    bias_1(4:6) = filtered_1(4:6);
+    bias_2(4:6) = filtered_2(4:6);
+    bias_3(4:6) = filtered_3(4:6);
+    
+    %%% earth magnetic field
+    ST = transpose(quaternion_rotmatrix(q)); % launch attitude  
+    if IMU_select(1) == 1 % only add alive IMUs to average
+        bias_1(7:9) = ST * filtered_1(7:9);
+        % TODO: add iron corrections. Maybe in IMU handler, next to rotation correction??
+    end
+    if IMU_select(2) == 1
+        bias_2(7:9) = ST * filtered_2(7:9);
+    end
+    if IMU_select(3) == 1
+        bias_3(7:9) = ST * filtered_3(7:9);
+    end
+
+    %%% barometer
+    % barometer bias not yet implemented, leave out for now.
+    % testing will show if we do pressure -> altitude, or if bias
+    % correction is needed when pressure varies wildly. Could be 
+    % (expected altitude on location) - (pressure -> altitude) -> (expected pressure)
+    % (barometer bias) = (pressure) - (expected pressure)
+
+end
+
