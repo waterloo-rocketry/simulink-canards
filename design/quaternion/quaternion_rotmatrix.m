68d72e6676533d2deeba13e471ae1ebf9bb48127
Finn Breuer <finn@tcrobotics.eu>
2025-04-07 00:55:04 +0200

    Jacobian for Quaternion rotation

--- design/quaternion/quaternion_rotmatrix.m
+++ design/quaternion/quaternion_rotmatrix.m
@@ -2,7 +2,7 @@ function [S] = quaternion_rotmatrix(q)
     % computes rotation matrix from quaternion
     
     %%% norm quaternions
-    q = 1/norm(q) * q;
+    % q = 1/norm(q) * q;
 
     %%% quaternion definition
     qw = q(1); qv = q(2:4);    
@@ -15,4 +15,8 @@ function [S] = quaternion_rotmatrix(q)
     %%% rotation matrix
     % S = (eye(3) + 2*q0*q_tilde + 2*q_tilde*q_tilde)'; Schiehlen
     S = (qw^2-qv'*qv)*eye(3) + 2*qv*qv' - 2*qw*q_tilde; % Stevens
+    %%% for hardcoding: 
+    % [qw^2 + qx^2 - qy^2 - qz^2,         2*qw*qz + 2*qx*qy,         2*qx*qz - 2*qw*qy]
+    % [        2*qx*qy - 2*qw*qz, qw^2 - qx^2 + qy^2 - qz^2,         2*qw*qx + 2*qy*qz]
+    % [        2*qw*qy + 2*qx*qz,         2*qy*qz - 2*qw*qx, qw^2 - qx^2 - qy^2 + qz^2]
 end
