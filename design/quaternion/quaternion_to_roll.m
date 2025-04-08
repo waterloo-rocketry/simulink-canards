8615609aa14c1cb7f0b474e29255adcdf1bb1e5c
Finn Breuer <finn@tcrobotics.eu>
2025-04-09 01:08:33 +0200

    By popular demand: dedicated quaternion2roll function

--- design/quaternion/quaternion_to_roll.m
+++ design/quaternion/quaternion_to_roll.m
@@ -1,4 +1,4 @@
-function [euler] = quaternion_to_euler(q)
+function [roll_angle] = quaternion_to_roll(q)
     % computes euler angles (yaw-pitch-roll sequence) from quaternion
     
     %%% norm quaternions
@@ -8,9 +8,9 @@ function [euler] = quaternion_to_euler(q)
     qw = q(1); qx = q(2); qy = q(3); qz = q(4);
 
     %%% Euler angles, after Zipfel
-    yaw = atan2( 2*(qx*qy + qw*qz), (qw^2+qx^2-qy^2-qz^2) );
-    pitch = asin( - 2*(qx*qz - qw*qy) );
+    % yaw = atan2( 2*(qx*qy + qw*qz), (qw^2+qx^2-qy^2-qz^2) );
+    % pitch = asin( - 2*(qx*qz - qw*qy) );
     roll = atan2( 2*(qy*qz + qw*qx), (qw^2-qx^2-qy^2+qz^2) );
 
-    euler = [roll; pitch; yaw];
+    roll_angle = roll;
 end
