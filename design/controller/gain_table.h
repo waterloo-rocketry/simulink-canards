5d2b168e172436dff2dc6e9715461dd5d32d732f
Finn Breuer <finn@tcrobotics.eu>
2025-04-09 18:42:54 +0200

    updated controller gains and gain files

---
+++ design/controller/gain_table.h
@@ -0,0 +1,31 @@
+#ifndef GAIN_TABLE_H_ 
+ #define GAIN_TABLE_H_
+
+/**
+ * Controller gains 
+
+ * Conversion from flight conditions to natural table coordinates:
+ * int x_nat = (int) (x_fc - x_OFFSET) / x_SCALE; 
+
+ * Array creation order: 
+ * for gain_number = 1:gain_amount 
+ *      for p = 1:P_size 
+ *          for c = 1:C_size 
+ *              Ks(p, c, gain_number)); 
+*/
+
+// Gain table information 
+#define GAIN_NUM 4;
+
+#define GAIN_P_SIZE 200;
+#define GAIN_C_SIZE 30;
+
+#define PRESSURE_DYNAMIC_SCALE 2.7512E+02;
+#define CANARD_COEFF_SCALE 5.0000E-01;
+
+#define PRESSURE_DYNAMIC_OFFSET 1.0000E+02;
+#define CANARD_COEFF_OFFSET -5.0000E+00;
+
+extern const float gain_table[GAIN_NUM][GAIN_P_SIZE * GAIN_C_SIZE];
+
+#endif // GAIN_TABLE_H_
--- design/controller/schedule_file_creator.m
+++ design/controller/schedule_file_creator.m
@@ -9,47 +9,58 @@ table = load("controller\gains.mat", "Ks", "info");
 gain_amount = size(table.Ks, 3);
 
 %% create and open .c file
-fid = fopen('design\controller\controller_gains.c', 'w');
+fid_h = fopen('design\controller\gain_table.h', 'w');
+fid_c = fopen('design\controller\gain_table.c', 'w');
 
-%% write file information
+%% h file: write file information
 %%% header
-fprintf(fid, '// Controller gains \n\n');
+fprintf(fid_h, '#ifndef GAIN_TABLE_H_ \n #define GAIN_TABLE_H_\n\n');
 
-fprintf(fid, '// Conversion from flight conditions to natural table coordinates:\n');
-fprintf(fid, '// int x_nat = (int) (x_fc - x_OFFSET) / x_SCALE; \n\n');
+fprintf(fid_h, '/**\n');
+fprintf(fid_h, ' * Controller gains \n\n');
 
-fprintf(fid, '// Array creation order: \n');
-fprintf(fid, '// for gain_number = 1:gain_amount \n');
-fprintf(fid, '//     for p = 1:P_size \n');
-fprintf(fid, '//         for c = 1:C_size \n');
-fprintf(fid, '//             Ks(p, c, gain_number)); \n\n');
+fprintf(fid_h, ' * Conversion from flight conditions to natural table coordinates:\n');
+fprintf(fid_h, ' * int x_nat = (int) (x_fc - x_OFFSET) / x_SCALE; \n\n');
+
+fprintf(fid_h, ' * Array creation order: \n');
+fprintf(fid_h, ' * for gain_number = 1:gain_amount \n');
+fprintf(fid_h, ' *      for p = 1:P_size \n');
+fprintf(fid_h, ' *          for c = 1:C_size \n');
+fprintf(fid_h, ' *              Ks(p, c, gain_number)); \n');
+fprintf(fid_h, '*/\n\n');
 
 %%% table information
-fprintf(fid, '// Gain table information \n');
-fprintf(fid, 'const int GAINS_AMOUNT = %d;\n\n', gain_amount);
-fprintf(fid, 'const int GAINS_P_SIZE = %d;\n', table.info.P_size);
-fprintf(fid, 'const int GAINS_C_SIZE = %d;\n\n', table.info.C_size);
-fprintf(fid, 'const float GAINS_P_SCALE = %.4E;\n', table.info.P_scale);
-fprintf(fid, 'const float GAINS_C_SCALE = %.4E;\n\n', table.info.C_scale);
-fprintf(fid, 'const float GAINS_P_OFFSET = %.4E;\n', table.info.P_offset);
-fprintf(fid, 'const float GAINS_C_OFFSET = %.4E;\n\n', table.info.C_offset);
+fprintf(fid_h, '// Gain table information \n');
+fprintf(fid_h, '#define GAIN_NUM %d;\n\n', gain_amount);
+fprintf(fid_h, '#define GAIN_P_SIZE %d;\n', table.info.P_size);
+fprintf(fid_h, '#define GAIN_C_SIZE %d;\n\n', table.info.C_size);
+
+fprintf(fid_h, '#define PRESSURE_DYNAMIC_SCALE %.4E;\n', table.info.P_scale);
+fprintf(fid_h, '#define CANARD_COEFF_SCALE %.4E;\n\n', table.info.C_scale);
+fprintf(fid_h, '#define PRESSURE_DYNAMIC_OFFSET %.4E;\n', table.info.P_offset);
+fprintf(fid_h, '#define CANARD_COEFF_OFFSET %.4E;\n\n', table.info.C_offset);
+
+%%% array and end
+fprintf(fid_h, 'extern const float gain_table[GAIN_NUM][GAIN_P_SIZE * GAIN_C_SIZE];\n\n');
+
+fprintf(fid_h, '#endif // GAIN_TABLE_H_');
 
 %% write gain array
-% for gain_number = 1:size(table.Ks, 3)
-%     write_array(fid, table.Ks, gain_number, table.info.P_size, table.info.C_size)
-% end
-write_array2(fid, table.Ks, gain_amount, table.info.P_size, table.info.C_size)
+fprintf(fid_c, '#include "application/controller/gain_table.h"\n\n');
+
+write_array2(fid_c, table.Ks, gain_amount, table.info.P_size, table.info.C_size)
 
 %% close file
-fclose(fid);
+fclose(fid_h);
+fclose(fid_c);
 
 %% write array function definition
 function [] = write_array2(fid, Ks, gain_amount, P_size, C_size)
-    fprintf(fid, '// Gain array \n');
-    fprintf(fid, 'const float gains[%d][%d] = \n{\n', gain_amount, P_size*C_size);
+    fprintf(fid, '// Gain table \n');
+    fprintf(fid, ['const float gain_table[GAIN_NUM][GAIN_P_SIZE * GAIN_C_SIZE] = {\n']);
     for gain_number = 1:gain_amount
         fprintf(fid, '    // Gain %d = \n', gain_number);
-        fprintf(fid, '    {\n     ');
+        fprintf(fid, '    {');
         for p = 1:P_size
             for c = 1:C_size
                 fprintf(fid, '%.4E', Ks(p, c, gain_number));
@@ -62,32 +73,10 @@ function [] = write_array2(fid, Ks, gain_amount, P_size, C_size)
             end
         end
         if gain_number < gain_amount
-            fprintf(fid, '\n    }, \n');
+            fprintf(fid, '},\n');
         else
-            fprintf(fid, '\n    } \n');
+            fprintf(fid, '}\n');
         end
     end
-    fprintf(fid, '};\n\n');
+    fprintf(fid, '};');
 end
-
-% %% write array function definition
-% function [] = write_array(fid, Ks, gain_number, P_size, C_size)
-%     fprintf(fid, '// Gain %d array \n', gain_number);
-%     fprintf(fid, 'const float gain_%d[%d][%d] = \n{\n', gain_number, P_size, C_size);
-%     for i = 1:P_size
-%         fprintf(fid, '    {');
-%         for j = 1:C_size
-%             fprintf(fid, '%.4E', Ks(i, j, gain_number));
-%             if j < C_size
-%                 fprintf(fid, ', ');
-%             end
-%         end
-%         fprintf(fid, '}');
-%         if i < P_size
-%             fprintf(fid, ',\n');
-%         else
-%             fprintf(fid, '\n');
-%         end
-%     end
-%     fprintf(fid, '};\n\n');
-% end
