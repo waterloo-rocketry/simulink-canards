7a3bb7f1fb89a95fc545962750e9d7953801c7eb
Finn Breuer <finn@tcrobotics.eu>
2025-04-15 21:57:57 +0200

    quaternion clean up

--- design/quaternion/quaternion_multiply.m
+++ design/quaternion/quaternion_multiply.m
@@ -1,9 +1,6 @@
 function [q_prod] = quaternion_multiply(q, r)
     % quaternion multiplication
     
-    %%% norm quaternions
-    % q = 1/norm(q) * q;
-
     % Quaternion product matrix
     Q = [q(1), -q(2), -q(3), -q(4);
          q(2), q(1), -q(4), q(3);
--- design/quaternion/quaternion_rotmatrix.m
+++ design/quaternion/quaternion_rotmatrix.m
@@ -13,8 +13,8 @@ function [S] = quaternion_rotmatrix(q)
               -q(3), q(2), 0];
     
     %%% rotation matrix
-    % S = (eye(3) + 2*q0*q_tilde + 2*q_tilde*q_tilde)'; Schiehlen
     S = (qw^2-qv'*qv)*eye(3) + 2*qv*qv' - 2*qw*q_tilde; % Stevens
+    
     %%% for hardcoding: 
     % [qw^2 + qx^2 - qy^2 - qz^2,         2*qw*qz + 2*qx*qy,         2*qx*qz - 2*qw*qy]
     % [        2*qx*qy - 2*qw*qz, qw^2 - qx^2 + qy^2 - qz^2,         2*qw*qx + 2*qy*qz]
