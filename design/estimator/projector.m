5f9c54368a46549b6793c83880473a9b57f81c32
Finn Breuer <finn@tcrobotics.eu>
2025-04-15 22:53:48 +0200

    fix syntax

--- design/estimator/projector.m
+++ design/estimator/projector.m
@@ -16,9 +16,9 @@ function [output] = projector(x)
 
     %% scheduling variables
     % calculate air data
-    model_airdata(alt).density;
+    airdata = model_airdata(alt);
     airspeed = norm(v);
-    p_dyn = rho/2*airspeed^2;
+    p_dyn = 0.5 * airdata.density * airspeed^2;
     
     % cat flight condition
     output(4:5,:) = [p_dyn; Cl];
