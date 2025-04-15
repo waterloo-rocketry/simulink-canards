6e9791380a361736625a79e7ff9a9e3ad7d76844
Finn Breuer <finn@tcrobotics.eu>
2025-04-15 22:37:42 +0200

    tuning

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -30,7 +30,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     % IMU 1 (MTi630)
     if IMU_select(1) == 1 % only correct with alive IMUs
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*1e-5, ones(1,3)*5e-2, 2e1]);
+        R = diag([ones(1,3)*1e-5, ones(1,3)*5e-3, 2e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:end), b.bias_1, R);
         x = xhat; P = Phat;
@@ -39,7 +39,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     % IMU 2 (AltIMU)
     if IMU_select(2) == 1
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*2e-5, ones(1,3)*1e-1, 3e1]);
+        R = diag([ones(1,3)*2e-5, ones(1,3)*1e-3, 3e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_2(4:end), b.bias_2, R);
         x = xhat; P = Phat;
@@ -49,9 +49,8 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     if 0 % only correct with alive IMUs
         %%% y = [   Q(4)    ]
         R = diag(ones(1,4) * 1e-2);
-        
-        % work in progress
-        %[xhat, Phat] = ekf_correct(@model_meas_ahrs, @model_meas_ahrs_jacobian, x, P, ahrs, b.bias_ahrs, R);
+        % the [0;0;0;0] argument should be replaced with the orientation correction 
+        % (if AHRS always initilizes the same) or with bias to correct in model
         [xhat, Phat] = ekf_correct(@model_meas_ahrs, @model_meas_ahrs_jacobian, x, P, AHRS, [0;0;0;0], R);
         x = xhat; P = Phat;
     end
