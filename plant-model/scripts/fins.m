3bf0a881230ed805217889e85237c266af5ec118
Tristan Yan-Klassen <emptrex@gmail.com>
2025-05-28 20:56:34 -0400

    Update fins.m

--- plant-model/scripts/fins.m
+++ plant-model/scripts/fins.m
@@ -1,4 +1,4 @@
-function [fin_pos_x_cp, Cnfdelta, CndNi, CNa_fins, fin_aspectratio, fin_area, fin_midchord_angle, fin_dist_chord_mean, fin_pos_r_chord_mean, fin_leading_edge] = fins(Cr, Ct, fin_height, sweep, pos_aletas, rt, N_fins, Ar, Lr, r0)
+function [fin_pos_x_cp, Cnfdelta, CndNi, fin_CNa, fin_aspectratio, fin_area, fin_midchord_angle, fin_dist_chord_mean, fin_pos_r_chord_mean, fin_leading_edge] = fins(Cr, Ct, fin_height, sweep, pos_aletas, rt, N_fins, Ar, Lr, r0)
 %Fin-related parameters
 fin_area = (Cr + Ct) * fin_height / 2; % fin area
 fin_aspectratio = 2 * (fin_height^2) / fin_area; % Fin Aspect Ratio
@@ -15,6 +15,6 @@ c3 = ((Cr + 3*Ct)/12) * (fin_height^3);
 fin_pos_x_cp = pos_aletas - (sweep / 3) * ( (Cr + 2 * Ct) / (Cr + Ct) ) + (1/6) * (Cr + Ct - Cr * Ct / (Cr + Ct)); % Fins center of pressure
 Cnfdelta = fin_number * fin_pos_r_chord_mean / fin_height; % roll forcing moment coefficient derivative, multiple by delta and Cnalfa1
 CndNi= (fin_number * (c1 + c2 + c3))/(Ar * (Lr)) ; %roll damping moment coefficient derivative (partial, uses real time numbers during simulation)
-CNa_fins = ((4 * fin_number * (fin_height / Lr) ^ 2) / (1 + sqrt(1 + (2 * fin_leading_edge / (Cr + Ct)) ^ 2))) * (1 + rt / (fin_height + rt));
-CNa_fins = CNa_fins * (1 + r0/(fin_height + r0)); %interference factor
+fin_Cna = ((4 * fin_number * (fin_height / Lr) ^ 2) / (1 + sqrt(1 + (2 * fin_leading_edge / (Cr + Ct)) ^ 2))) * (1 + rt / (fin_height + rt));
+fin_CNa = fin_CNa * (1 + r0/(fin_height + r0)); %interference factor
 end
