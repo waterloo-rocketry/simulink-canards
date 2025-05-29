d2a0ba4289d5c4efb9393387a21b052298b2155e
Tristan Yan-Klassen <emptrex@gmail.com>
2025-05-28 20:28:21 -0400

    some fixes

--- plant-model/scripts/aerosurface.m
+++ plant-model/scripts/aerosurface.m
@@ -1,7 +1,8 @@
 function [] = aerosurface(chord_root, chord_tip, height, sweep, number, rocket_diameter)
     % calculates coefficients and other parameters for aerosurfaces, such
     % as fins or canards. The aerosurface must be trapezoidal (for triangle: short tip chord)
-
+    %Output names: fin_ or canard_ [pos_x_cp, CNf_delta, CNdNi, CNa,
+    %aspect_ratio, area, midchord_angle, pos_r_chord_mean, leading_edge]
     area_planform = height * (chord_root + chord_tip) / 2;
     chord_aerodynamic_mean = 2/3 * (chord_root + chord_tip - (chord_root*chord_tip)/(chord_root+chord_tip));
     height_aerodynamic_mean = height / 3 * (chord_root + 2*chord_tip) / (chord_root + chord_tip);
--- plant-model/scripts/fins.m
+++ plant-model/scripts/fins.m
@@ -1,11 +1,11 @@
-function [fin_pos_x_cp, Cnfdelta, CndNi, CNa_fins, fin_aspectratio, fin_area, fin_midchord_angle, fin_pos_r_chord_mean, Y, Lf] = fins(Cr, Ct, fin_height, sweep, pos_aletas, rt, N_fins, Ar, Lr, r0)
+function [fin_pos_x_cp, Cnfdelta, CndNi, CNa_fins, fin_aspectratio, fin_area, fin_midchord_angle, fin_dist_chord_mean, fin_pos_r_chord_mean, fin_leading_edge] = fins(Cr, Ct, fin_height, sweep, pos_aletas, rt, N_fins, Ar, Lr, r0)
 %Fin-related parameters
 fin_area = (Cr + Ct) * fin_height / 2; % fin area
 fin_aspectratio = 2 * (fin_height^2) / fin_area; % Fin Aspect Ratio
 fin_midchord_angle = atan( sweep / (2 * fin_height) ); % mid chord angle
 fin_dist_chord_mean = (Cr + 2 * Ct) / (Cr + Ct); %mean aerodynamic chord distance
 fin_pos_r_chord_mean = rt + (fin_height/3) * fin_dist_chord_mean; %mean aerodynamic chord distance with the radius added
-Lf = sqrt((sweep / 2) ^ 2 + fin_height ^ 2); % Pre calculus. No Physical meaning
+fin_leading_edge = sqrt((sweep / 2) ^ 2 + fin_height ^ 2); % Pre calculus. No Physical meaning
 
 % Pre calculus for extense calculations
 c1 = ((Cr + Ct) /  2) * (rt^2) * fin_height;
@@ -15,6 +15,6 @@ c3 = ((Cr + 3*Ct)/12) * (fin_height^3);
 fin_pos_x_cp = pos_aletas - (sweep / 3) * ( (Cr + 2 * Ct) / (Cr + Ct) ) + (1/6) * (Cr + Ct - Cr * Ct / (Cr + Ct)); % Fins center of pressure
 Cnfdelta = fin_number * fin_pos_r_chord_mean / fin_height; % roll forcing moment coefficient derivative, multiple by delta and Cnalfa1
 CndNi= (fin_number * (c1 + c2 + c3))/(Ar * (Lr)) ; %roll damping moment coefficient derivative (partial, uses real time numbers during simulation)
-CNa_fins = ((4 * fin_number * (fin_height / Lr) ^ 2) / (1 + sqrt(1 + (2 * Lf / (Cr + Ct)) ^ 2))) * (1 + rt / (fin_height + rt));
+CNa_fins = ((4 * fin_number * (fin_height / Lr) ^ 2) / (1 + sqrt(1 + (2 * fin_leading_edge / (Cr + Ct)) ^ 2))) * (1 + rt / (fin_height + rt));
 CNa_fins = CNa_fins * (1 + r0/(fin_height + r0)); %interference factor
 end
