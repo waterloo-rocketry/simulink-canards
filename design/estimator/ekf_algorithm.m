a105adbfce6edbe83ce2b5ca0146456b6aa12388
Finn Breuer <finn@tcrobotics.eu>
2025-06-27 13:11:34 -0400

    Better velocity estimates

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -9,7 +9,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     %% Prediction step
     %%% Q is a square 13 matrix, tuning for prediction E(noise)
     %%% x = [   q(4),           w(3),           v(3),      alt(1), Cl(1), delta(1)]
-    Q = diag([ones(1,4)*1e-9, ones(1,3)*1e0, ones(1,3)*1e-2, 1e-2,  40, 0.5]);
+    Q = diag([ones(1,4)*1e-9, ones(1,3)*1e-2, ones(1,3)*1e-3, 1e-3,  30, 0.5]);
     
     u.accel = model_acceleration(x, IMU_1, IMU_2);
     u.cmd = cmd;
@@ -30,7 +30,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     % IMU 1 (MTi630)
     if IMU_select(1) == 1 % only correct with alive IMUs
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*1e-6, ones(1,3)*2e-3, 2e1]);
+        R = diag([ones(1,3)*2e-7, ones(1,3)*2e-3, 2e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:end), b.bias_1, R);
         x = xhat; P = Phat;
@@ -39,7 +39,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, AHRS)
     % IMU 2 (AltIMU)
     if IMU_select(2) == 1
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*2e-6, ones(1,3)*1e-3, 3e1]);
+        R = diag([ones(1,3)*5e-7, ones(1,3)*1e-3, 3e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_2(4:end), b.bias_2, R);
         x = xhat; P = Phat;
