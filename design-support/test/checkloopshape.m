9257a329d1ef153e5dffce372b1f2d456d5d1464
Finn Breuer <finn@tcrobotics.eu>
2025-07-09 01:33:24 -0400

    check open cut loop

--- design-support/test/checkloopshape.m
+++ design-support/test/checkloopshape.m
@@ -14,9 +14,23 @@
 function []=checkloopshape(L,Llbd,wlbd,Lubd,wubd)
 
 % ------ preparations ----------
-Nw = 1000;
-w = logspace(-3,2,Nw)';
 
+log_min = -2;
+log_max = 3;
+
+Ts = L.Ts;
+if Ts ~= 0
+    w_sample = pi/Ts; 
+    log_max = log10(w_sample);
+end
+
+xmin = 10^(log_min);
+xmax = 10^(log_max);
+ymin = -50;
+ymax = 50;
+
+Nw = 1000;
+w = logspace(log_min,log_max,Nw)';
 Lmag = bode(L,w);
 Lmag = Lmag(:);
 LmagdB = 20*log10(Lmag);
@@ -26,30 +40,28 @@ wL = w(Lidx);
 idx = (w >= wubd);
 idxwubd = find(idx,1,'first');
 
-xmin = 1e-3;
-xmax = 100;
-ymin = -50;
-ymax = 50;
 % ------------------------------
 
-lhL = semilogx([wL wL xmin],[ymin 0 0],'k');
+% semilogx(w,LmagdB,'k','LineWidth',0.5);
+semilogx(w,LmagdB,'LineWidth',0.5);
 hold on;
-phLclb  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)],...
-    [10 0 -15 ymin ymin ymin],'g');
-phLcub  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)],...
-    [15 0 -10 ymax ymax ymax],'g');
-semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[10 0 -15]);%,'g');
-semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[15 0 -10]);%,'g');
-semilogx(w,LmagdB,'k','LineWidth',0.5);
 axis([xmin xmax ymin ymax]);
-phLlb = patch([w(1) wlbd wlbd w(1)],[ymin ymin Llbd Llbd],'b');
-phLub = patch([w(end) wubd wubd w(end)],[ymax ymax Lubd LmagdB(idxwubd)-20],'r');
+semilogx([w(1) w(end)],[0 0],'k');
+lhL = semilogx([wL wL xmin],[ymin 0 0],'k:');
+% phLclb  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)], [10 0 -15 ymin ymin ymin],'g');
+% phLcub  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)], [15 0 -10 ymax ymax ymax],'g');
+semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[10 0 -15],'g--');
+semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[15 0 -10],'g--');
+% phLlb = patch([w(1) wlbd wlbd w(1)],[ymin ymin Llbd Llbd],'b');
+% phLub = patch([w(end) wubd wubd w(end)],[ymax ymax Lubd LmagdB(idxwubd)-20],'r');
 semilogx([w(1) wlbd],[Llbd Llbd],'b');
-semilogx([wubd w(end)],[Lubd Lubd-20],'r');
-set(phLlb,'FaceAlpha',0.15,'LineStyle','none');
-set(phLub,'FaceAlpha',0.15,'LineStyle','none');
-set(phLclb,'FaceAlpha',0.15,'LineStyle','none');
-set(phLcub,'FaceAlpha',0.15,'LineStyle','none');
+semilogx([wlbd wlbd],[ymin Llbd],'b');
+semilogx([wubd w(end)],[Lubd -40],'r');
+semilogx([wubd wubd],[Lubd ymax],'r');
+% set(phLlb,'FaceAlpha',0.15,'LineStyle','none');
+% set(phLub,'FaceAlpha',0.15,'LineStyle','none');
+% set(phLclb,'FaceAlpha',0.15,'LineStyle','none');
+% set(phLcub,'FaceAlpha',0.15,'LineStyle','none');
 xlabel('Frequency, rad/sec');
 ylabel('|L|=|GK|, dB');
 hold off
--- design-support/test/test_bandwidth.m
+++ design-support/test/test_bandwidth.m
@@ -1,38 +1,73 @@
-%% state
-q = [1; 0; 0; 0]; % orientation
-w = [0.01; 0.01; 0.01]; % rates
-v = [600; 1; 5]; % velocity
-alt = 200; % altitude
-CL = 1.5; % canard coefficient
-delta = 0.0; % canard deflection 
+%% initials
+CL = 4; % canard coefficient
+alt = 1000; % altitude for dyn pressure
+rho = model_airdata(alt).density;
 
-x = [q; w; v; alt; CL; delta];
+V = linspace(20, 900, 40);
+P = 0.5 * rho * V.^2;
+steptime = 20;
+T_sample = 0.01; % sampling time of the loop
 
-u = 0.1; % control input 
-A = [10; 2; 1]; % accelerometer measurement
+clear control_scheduler
+clear model_roll
 
-u_ekf = cat(1, u, A);
+%% test lqr + step
+for i=1:length(P)   
+    Ks = control_scheduler([P(i), CL]);
+    K_pre = Ks(end);
+    K = Ks(1:end-1);
 
-% compute rotational matrix (attitude transformation matrix, between body frame and ground frame)
-S = model_quaternion_rotmatrix(q);
+    [A, B, C, ~] = model_roll(P(i), CL);
+    sys_plant = c2d(ss(A, B, eye(3), 0), T_sample);
+    [phi, gamma] = ssdata(sys_plant);
 
-% compute roll angle       
-phi = atan2(S(2,3)/S(3,3)); % double check if this is the correct angle
+    %%% rolloff filter
+    f_rolloff = 100; % [rad/s] rolloff frequency
+    lowpass = c2d(tf(f_rolloff, [1, f_rolloff]), T_sample);
 
-% cat roll state
-x_roll = [phi; w(1); delta];
+    sys_ol = K_pre * K * ss(phi, gamma, eye(3), 0, T_sample);
+    % sys_ol = K_pre * K * ss(phi, gamma, eye(3), 0, T_sample) * lowpass;
+    sys_cl = K_pre * ss(phi+gamma*K, gamma, eye(3), 0, T_sample);
+    
 
-%% test lqr
-Q = diag([10, 0, 10]);
-R = 1e4;
-K= lqr_tune(x, Q, R)
+    sys_array(:,:,1,i) = sys_cl;
+    sys_array_open(:,:,1,i) = sys_ol;
 
-[A, B, C, ~] = model_roll(x);
+    if i == 1
+        sys_min = sys_cl;
+    elseif i == length(P)
+        sys_max = sys_cl;
+    end
+    % step(sys_cl, steptime);
+    % hold on
 
-sys_cl = ss(A+B*K, B, C, 0);
-K_pre = 1/dcgain(sys_cl(1));
-sys_cl = K_pre*sys_cl;
 
-bode(sys_cl)
+end
 
-wb = bandwidth(sys_cl(1))
+%% Figure
+
+w_low = 0.1;
+L_low = 20;
+
+w_high = 2*pi*10;
+L_high = -20;
+
+figure(1)
+for i = 1 : 3 : length(P)   
+    checkloopshape(sys_array_open(1,1,1,i), L_low, w_low, L_high, w_high)
+    % checkloopshape(L,Llbd,wlbd,Lubd,wubd)
+    % inputs:   L -> loop transfer function (must be siso!)
+    %           Llbd -> in db, lower bound for the loop gain at low frequencies
+    %           wlbd -> in rad/s, frequency below which |L| shall be larger than |Llbd|
+    %           Lubd -> in db, upper bound for the loop gain at high frequencies
+    %           wubd -> in rad/s, frequency above which |L| shall be less than |Lubd|
+    hold on
+end
+checkloopshape(ss(phi, gamma, [1,0,0], 0, T_sample),  L_low, w_low, L_high, w_high)
+hold off
+% hold on
+% bode(sys_min(1,1), 'g', sys_max(1,1), 'r')
+% hold off
+
+% [margin1, margin2] = margin(sys_array(1,1))
+% [margin] = margin(sys_min(1,1), 'g', sys_max(1,1), 'r')
