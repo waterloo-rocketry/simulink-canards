2790bf78bb4204c41f13482b17b7b5b8745e09b5
Finn Breuer <finn@tcrobotics.eu>
2025-07-09 09:20:07 -0400

    changed paths to use forward slash

--- plant-model/configure_plant_model.m
+++ plant-model/configure_plant_model.m
@@ -1,22 +1,22 @@
 clear
 %% Choose rocket
-% run('plant-model\Data\Borealis\borealis.m')
-% run('plant-model\Data\testflight\testflight.m')
-run('plant-model\Data\Aurora\aurora.m')
+% run('plant-model/Data/Borealis/borealis.m')
+% run('plant-model/Data/testflight/testflight.m')
+run('plant-model/Data/Aurora/aurora.m')
 
 %%% environment
-run('plant-model\Data\Environment\environment.m')
+run('plant-model/Data/Environment/environment.m')
 % enable wind disturbances
 wind_dist_enable = 1; % no disturbances is = 0
 
 %%% sensors
-run('plant-model\Data\Sensors\sensors_processing.m')
+run('plant-model/Data/Sensors/sensors_processing.m')
 
 %% data pre-processing
-run('plant-model\scripts\data_preparation.m')
+run('plant-model/scripts/data_preparation.m')
 
 
 %% Controller and Estimator loading
 clear estimator_module
 clear control_scheduler
-design_param = load("model\model_params.mat");
+design_param = load("model/model_params.mat");
