7c119d3d97ba58e2b7208f672f7f60f0becefcc8
Finn Breuer <finn@tcrobotics.eu>
2025-07-09 16:19:57 -0400

    fixed post-processor

--- monte-carlo/sim_postprocessor.m
+++ monte-carlo/sim_postprocessor.m
@@ -4,45 +4,45 @@ function [sdt, sdt_vars] = sim_postprocessor(simout)
     % Output parameters: sdt (struct with timetables of combined data), sdt_vars (struct with timetables of individual variables)
     
     %%% Controller data
-    sdt_vars.ref = sim_getdata(simout, 'ref', 1);
-    sdt_vars.control = sim_getdata(simout, 'controlinput', 5);
-    sdt_vars.cmd = sim_getdata(simout, 'cmd', 1);
+    sdt_vars.ref = sim_getdata(simout, "ref", 1);
+    sdt_vars.control = sim_getdata(simout, "controlinput", 5);
+    sdt_vars.cmd = sim_getdata(simout, "cmd", 1);
     
-    sdt_vars.roll = timetable(sdt_vars.control.Time, sdt_vars.control.controlinput(:,1), 'VariableNames', 'roll');
-    sdt_vars.rollerr = sim_ttdiff(sdt_vars.ref, sdt_vars.roll, 'err');
+    sdt_vars.roll = timetable(sdt_vars.control.Time, sdt_vars.control.controlinput(:,1), 'VariableNames', "roll");
+    sdt_vars.rollerr = sim_ttdiff(sdt_vars.ref, sdt_vars.roll, "err");
     sdt.control = synchronize(sdt_vars.ref, sdt_vars.roll, sdt_vars.rollerr);
     
     %%% Rocket data
-    sdt_vars.q = sim_getdata(simout, 'q', 4);
-    sdt_vars.w = sim_getdata(simout, 'w', 3);
-    sdt_vars.v = sim_getdata(simout, 'v', 3);
-    sdt_vars.pos = sim_getdata(simout, 'pos', 3);
-        sdt_vars.alt = timetable(sdt_vars.pos.Time, sdt_vars.pos.pos(:,1), 'VariableNames', 'alt');
-    sdt_vars.cl = sim_getdata(simout, 'CL', 1);
-    sdt_vars.delta = sim_getdata(simout, 'delta', 1);
+    sdt_vars.q = sim_getdata(simout, "q", 4);
+    sdt_vars.w = sim_getdata(simout, "w", 3);
+    sdt_vars.v = sim_getdata(simout, "v", 3);
+    sdt_vars.pos = sim_getdata(simout, "pos", 3);
+        sdt_vars.alt = timetable(sdt_vars.pos.Time, sdt_vars.pos.pos(:,1), 'VariableNames', "alt");
+    sdt_vars.cl = sim_getdata(simout, "CL", 1);
+    sdt_vars.delta = sim_getdata(simout, "delta", 1);
     sdt.rocket = synchronize(sdt_vars.q, sdt_vars.w, sdt_vars.v, sdt_vars.alt, sdt_vars.cl, sdt_vars.delta);
-        sdt.rocket = renamevars(sdt.rocket, 1:6, ['q', 'w', 'v', 'alt', 'cl', 'delta']);
+        sdt.rocket = renamevars(sdt.rocket, 1:6, ["q", "w", "v", "alt", "cl", "delta"]);
     
     %%% Estimator data
-    sdt_vars.qhat = sim_getdata(simout, 'q_hat', 4);
-    sdt_vars.what = sim_getdata(simout, 'w_hat', 3);
-    sdt_vars.vhat = sim_getdata(simout, 'v_hat', 3);
-    sdt_vars.althat = sim_getdata(simout, 'alt_hat', 1);
-    sdt_vars.clhat = sim_getdata(simout, 'cl_hat', 1);
-    sdt_vars.deltahat = sim_getdata(simout, 'delta_hat', 1);
+    sdt_vars.qhat = sim_getdata(simout, "q_hat", 4);
+    sdt_vars.what = sim_getdata(simout, "w_hat", 3);
+    sdt_vars.vhat = sim_getdata(simout, "v_hat", 3);
+    sdt_vars.althat = sim_getdata(simout, "alt_hat", 1);
+    sdt_vars.clhat = sim_getdata(simout, "cl_hat", 1);
+    sdt_vars.deltahat = sim_getdata(simout, "delta_hat", 1);
     sdt.est = synchronize(sdt_vars.qhat, sdt_vars.what, sdt_vars.vhat, sdt_vars.althat, sdt_vars.clhat, sdt_vars.deltahat);
-        sdt.est = renamevars(sdt.est, 1:6, ['q', 'w', 'v', 'alt', 'cl', 'delta']);
+        sdt.est = renamevars(sdt.est, 1:6, ["q", "w", "v", "alt", "cl", "delta"]);
     
-    sdt_vars.qerr = sim_ttdiff(sdt_vars.q, sdt_vars.qhat, 'err');
-    sdt_vars.werr = sim_ttdiff(sdt_vars.w, sdt_vars.what, 'err');
-    sdt_vars.verr = sim_ttdiff(sdt_vars.v, sdt_vars.vhat, 'err');
-    sdt_vars.alterr = sim_ttdiff(sdt_vars.alt, sdt_vars.althat, 'err'); 
-    sdt_vars.clerr = sim_ttdiff(sdt_vars.cl, sdt_vars.clhat, 'err');
-    sdt_vars.deltaerr = sim_ttdiff(sdt_vars.delta, sdt_vars.deltahat, 'err');
+    sdt_vars.qerr = sim_ttdiff(sdt_vars.q, sdt_vars.qhat, "err");
+    sdt_vars.werr = sim_ttdiff(sdt_vars.w, sdt_vars.what, "err");
+    sdt_vars.verr = sim_ttdiff(sdt_vars.v, sdt_vars.vhat, "err");
+    sdt_vars.alterr = sim_ttdiff(sdt_vars.alt, sdt_vars.althat, "err"); 
+    sdt_vars.clerr = sim_ttdiff(sdt_vars.cl, sdt_vars.clhat, "err");
+    sdt_vars.deltaerr = sim_ttdiff(sdt_vars.delta, sdt_vars.deltahat, "err");
     sdt.error = synchronize(sdt_vars.qerr, sdt_vars.werr, sdt_vars.verr, sdt_vars.alterr, sdt_vars.clerr, sdt_vars.deltaerr);
-        sdt.error = renamevars(sdt.error, 1:6, ['q', 'w', 'v', 'alt', 'cl', 'delta']);
+        sdt.error = renamevars(sdt.error, 1:6, ["q", "w", "v", "alt", "cl", "delta"]);
 
-    sdt_vars.P_norm = sim_getdata(simout, 'P_norm', 3);
+    sdt_vars.P_norm = sim_getdata(simout, "P_norm", 3);
 
     % Rocket data in estimator time code
     sdt.rocket_dt = retime(sdt.rocket, sdt.est.Time);
