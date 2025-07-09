1e57dd986471e9bc24c93d697e33e949958135b9
Finn Breuer <finn@tcrobotics.eu>
2025-07-09 18:05:50 -0400

    sim build debug mode

--- design/estimator/imu_handler.m
+++ design/estimator/imu_handler.m
@@ -15,10 +15,10 @@ function [IMU_1, IMU_2, IMU_3] = imu_handler(IMU_1, IMU_2, IMU_select)
     for k = 1:2
         if IMU_select(1) == 1 % use only alive IMUs
             % rotate into body coordinates from sensor coordinate
-            IMU_1( 1+3*(k-1) : 3*k ) = param.S_k(:,:,1) * IMU_1( 1+3*(k-1) : 3*k );
+            IMU_1( 3*k-2 : 3*k ) = param.S_k(:,:,1) * IMU_1( 3*k-2 : 3*k );
         end
         if IMU_select(2) == 1
-            IMU_2( 1+3*(k-1) : 3*k ) = param.S_k(:,:,2) * IMU_2( 1+3*(k-1) : 3*k );
+            IMU_2( 3*k-2 : 3*k ) = param.S_k(:,:,2) * IMU_2( 3*k-2 : 3*k );
         end
     end
 end
