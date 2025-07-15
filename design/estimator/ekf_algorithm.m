f4c38f03dc245794ff088a415017743212dfa594
Finn Breuer <finn@tcrobotics.eu>
2025-07-14 22:41:29 -0400

    tuning for robustness

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -9,7 +9,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
     %% Prediction step
     %%% Q is a square 13 matrix, tuning for prediction E(noise)
     %%% x = [   q(4),           w(3),           v(3),      alt(1), Cl(1), delta(1)]
-    Q = diag([ones(1,4)*1e-9, ones(1,3)*1e-2, ones(1,3)*1e-3, 1e-3,  30, 0.5]);
+    Q = diag([ones(1,4)*1e-10, ones(1,3)*1e-2, ones(1,3)*1e-2, 1e-3,  30, 0.5]);
     
     u.accel = model_acceleration(x, IMU_1, IMU_2, sensor_select);
     u.cmd = cmd;
@@ -18,8 +18,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
 
     %% Correction step(s), sequential for each IMU
     %%% R is a square matrix (size depending on amount of sensors), tuning for measurement E(noise)
-    
-    
+     
     % Encoder
     if sensor_select(3) == 1
         %%% y = [enc(1)]
@@ -31,7 +30,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
     % IMU 1 (MTi630)
     if sensor_select(1) == 1 % only correct with alive IMUs
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*2e-7, ones(1,3)*2e-3, 2e1]);
+        R = diag([ones(1,3)*2e-8, ones(1,3)*2e-3, 2e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:end), b.bias_1, R);
         x = xhat; P = Phat;
@@ -40,11 +39,10 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
     % IMU 2 (AltIMU)
     if sensor_select(2) == 1
         %%% y = [   W(3),          Mag(3),      P(1)]
-        R = diag([ones(1,3)*5e-7, ones(1,3)*1e-3, 3e1]);
+        R = diag([ones(1,3)*3e-8, ones(1,3)*1e-3, 3e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_2(4:end), b.bias_2, R);
         x = xhat; P = Phat;
     end 
-
 
 end
