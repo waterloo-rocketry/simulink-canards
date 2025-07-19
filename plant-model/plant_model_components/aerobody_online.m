817a98f44a37f906b68232b21df1229f04e40bb8
Finn Breuer <finn@tcrobotics.eu>
2025-07-19 19:34:05 -0400

    fixed aerobody computations

--- plant-model/plant_model_components/aerobody_online.m
+++ plant-model/plant_model_components/aerobody_online.m
@@ -1,11 +1,6 @@
 function [C_normal_1, C_m_1, cp] = aerobody_online(mach, alpha, area_bow, area_aft, area_planform, length, volume, x_cp)
 
     %%% Normal force coefficient 
-    gamma = 1.4; % adiabatic exponent of air
-
-    % threshold values
-    subsonic = 0.85;
-    supersonic = 1.6;
 
     % prandtl factor
     % beta = max(sqrt(abs(1-mach^2)), 0.4);
