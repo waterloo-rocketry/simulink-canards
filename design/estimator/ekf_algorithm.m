fd2f8b527bd976594ac8b8c32ceb5e84164eff82
Finn Breuer <finn@tcrobotics.eu>
2025-07-22 22:08:41 -0400

    Fixed pass of sensor_select

--- design/estimator/ekf_algorithm.m
+++ design/estimator/ekf_algorithm.m
@@ -11,7 +11,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
     %%% x = [   q(4),           w(3),           v(3),      alt(1), Cl(1), delta(1)]
     Q = diag([ones(1,4)*1e-10, ones(1,3)*1e-2, ones(1,3)*1e-2, 1e-3,  30, 0.5]);
     
-    u.accel = model_acceleration(x, IMU_1, IMU_2, sensor_select);
+    u.accel = model_acceleration(x, IMU_1, IMU_2, sensor_select(1:2));
     u.cmd = cmd;
     [xhat, Phat] = ekf_predict(@model_dynamics, @model_dynamics_jacobian, x, P, u, Q, T);
     x = xhat; P = Phat;
@@ -19,20 +19,20 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, cmd, encoder, senso
     %% Correction step(s), sequential for each IMU
     %%% R is a square matrix (size depending on amount of sensors), tuning for measurement E(noise)
      
-    % Encoder
-    if sensor_select(3) == 1
-        %%% y = [enc(1)]
-        R = 0.002;
-        [xhat, Phat] = ekf_correct(@model_meas_encoder, @model_meas_encoder_jacobian, x, P, encoder, 0, R);
-        x = xhat; P = Phat;
-    end
-
     % IMU 1 (MTi630)
     if sensor_select(1) == 1 % only correct with alive IMUs
         %%% y = [   W(3),          Mag(3),      P(1)]
         R = diag([ones(1,3)*2e-8, ones(1,3)*2e-3, 2e1]);
 
         [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:end), b.bias_1, R);
+        x = xhat; P = Phat;
+    end
+
+    % Encoder
+    if sensor_select(3) == 1
+        %%% y = [enc(1)]
+        R = 0.002;
+        [xhat, Phat] = ekf_correct(@model_meas_encoder, @model_meas_encoder_jacobian, x, P, encoder, 0, R);
         x = xhat; P = Phat;
     end
 
