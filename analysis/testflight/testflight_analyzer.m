2a323ed34d44e6a31e6698937682a43541a6a600
Finn Breuer <finn@tcrobotics.eu>
2025-08-01 18:58:51 -0400

    testflight save figures

--- analysis/testflight/testflight_analyzer.m
+++ analysis/testflight/testflight_analyzer.m
@@ -1,11 +1,11 @@
 % Read data (if not already in workspace)
-T_long = readtable('analysis/testflightomnibusloganal.xlsx', 'Sheet', 'proc_state_est'); 
+T_long = readtable('analysis/testflight/testflightomnibusloganal.xlsx', 'Sheet', 'proc_state_est'); 
 
 % relevant times only
 % keep only times between time_start and time_end
 time_start = 1455; % ekf starts at 1403, liftoff around 1463
 time_end = 1486;
-time_end = 1490;
+time_end = 1500;
 
 T_long.Var4 = [];
 T_long.notes = [];
@@ -40,21 +40,22 @@ for i=1:height(T)
 end
 
 %% plot
-set(groot, 'defaultAxesTickLabelInterpreter','latex')
-set(groot, 'defaultLegendInterpreter','latex')
-set(groot, 'DefaultTextInterpreter', 'latex')
+% set(groot, 'defaultAxesTickLabelInterpreter','latex')
+% set(groot, 'defaultLegendInterpreter','latex')
+% set(groot, 'DefaultTextInterpreter', 'latex')
 
-figure(1)
-stairs(T.timestamp_s, T.ATT_Q0, 'DisplayName', '$q_w$'); hold on;
-stairs(T.timestamp_s, T.ATT_Q1, 'DisplayName', '$q_x$');
-stairs(T.timestamp_s, T.ATT_Q2, 'DisplayName', '$q_y$');
-stairs(T.timestamp_s, T.ATT_Q3, 'DisplayName', '$q_z$');
+f_q = figure(1);
+stairs(T.timestamp_s, T.ATT_Q0, 'DisplayName', 'w'); hold on;
+stairs(T.timestamp_s, T.ATT_Q1, 'DisplayName', 'x');
+stairs(T.timestamp_s, T.ATT_Q2, 'DisplayName', 'y');
+stairs(T.timestamp_s, T.ATT_Q3, 'DisplayName', 'z');
 xlabel("Time [s]")
 ylabel("Quaternion [ ]")
+ylim([-1, 1])
 % title("Attitude quaternion") 
 legend('Location','southwest'); hold off;
 
-figure(2)
+f_e = figure(2);
 stairs(T.timestamp_s, T.euler_roll, 'DisplayName', 'roll'); hold on;
 stairs(T.timestamp_s, T.euler_pitch, 'DisplayName', 'pitch');
 stairs(T.timestamp_s, T.euler_yaw, 'DisplayName', 'yaw');
@@ -63,25 +64,25 @@ ylabel("Angle [rad]")
 % title("Relative Euler angles")
 legend('Location','southwest'); hold off;
 
-figure(3)
-stairs(T.timestamp_s, T.RATE_WX, 'DisplayName', '$\omega_x$'); hold on;
-stairs(T.timestamp_s, T.RATE_WY, 'DisplayName', '$\omega_y$')
-stairs(T.timestamp_s, T.RATE_WZ, 'DisplayName', '$\omega_z$')
+f_w = figure(3);
+stairs(T.timestamp_s, T.RATE_WX, 'DisplayName', 'x'); hold on;
+stairs(T.timestamp_s, T.RATE_WY, 'DisplayName', 'y')
+stairs(T.timestamp_s, T.RATE_WZ, 'DisplayName', 'z')
 xlabel("Time [s]")
 ylabel("Anglular rate [rad/s]")
 % title("Angular rates")
 legend('Location','northwest'); hold off;
 
-figure(4)
-stairs(T.timestamp_s, T.VEL_VX, 'DisplayName', '$v_x$'); hold on;
-stairs(T.timestamp_s, T.VEL_VY, 'DisplayName', '$v_y$');
-stairs(T.timestamp_s, T.VEL_VZ, 'DisplayName', '$v_z$');
+f_v = figure(4);
+stairs(T.timestamp_s, T.VEL_VX, 'DisplayName', 'x'); hold on;
+stairs(T.timestamp_s, T.VEL_VY, 'DisplayName', 'y');
+stairs(T.timestamp_s, T.VEL_VZ, 'DisplayName', 'z');
 xlabel("Time [s]")
 ylabel("Velocity [m/s]")
 % title("Velocity")
 legend('Location','best'); hold off;
 
-figure(5)
+f_a = figure(5);
 stairs(T.timestamp_s, T.ALT, 'DisplayName', 'alt')
 xlabel("Time [s]")
 ylabel("Altitude [m]")
@@ -89,15 +90,23 @@ ylabel("Altitude [m]")
 %legend(); 
 hold off;
 
-figure(6)
-stairs(T.timestamp_s, rad2deg(T.CANARD_ANGLE), 'DisplayName', '$\delta$'); hold on;
-stairs(T.timestamp_s, T.COEFF_CL, 'DisplayName', '$C_L$')
+f_c = figure(6);
+stairs(T.timestamp_s, rad2deg(T.CANARD_ANGLE), 'DisplayName', '\delta'); hold on;
+stairs(T.timestamp_s, T.COEFF_CL, 'DisplayName', 'C_L')
 xlabel("Time [s]")
 ylabel("Angle [deg], Coefficient [ ]")
 ylim([-1,5])
 % title("Canard")
 legend('Location','best'); hold off;
 
-set(groot, 'defaultAxesTickLabelInterpreter','remove')
-set(groot, 'defaultLegendInterpreter','remove')
-set(groot, 'DefaultTextInterpreter', 'remove')
+% set(groot, 'defaultAxesTickLabelInterpreter','remove')
+% set(groot, 'defaultLegendInterpreter','remove')
+% set(groot, 'DefaultTextInterpreter', 'remove')
+
+
+exportgraphics(f_q, 'analysis/testflight/testflight_q.png')
+exportgraphics(f_e, 'analysis/testflight/testflight_euler.png')
+exportgraphics(f_w, 'analysis/testflight/testflight_w.png')
+exportgraphics(f_v, 'analysis/testflight/testflight_v.png')
+exportgraphics(f_a, 'analysis/testflight/testflight_alt.png')
+exportgraphics(f_c, 'analysis/testflight/testflight_canard.png')
